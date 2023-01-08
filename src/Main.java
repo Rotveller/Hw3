147b4a9a76d5f16dc4b976922fb1866e05349084
cahas <bulatovalexsandr1999@gmail.com>
2023-01-08 12:13:24 +0400

    Done All Tasks

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
         task2 ();
         task3 ();
         task4 ();
-        task5();
-        ///Task6 ();
-        ///Task7 ();
-        ///Task8 ();
+        task5 ();
+        task6 ();
+        task7 ();
+        task8 ();
     }
     public static void task1 () {
         int one = 1;
@@ -56,9 +56,54 @@ public class Main {
         byte onePlaceWhite = 2;
         byte onePlaceBrown = 4;
         byte howClasR = (byte) (fullSc / (onePlaceWhite+onePlaceBrown));
-        System.out.println(howClasR);
+        System.out.println(" В школе " + howClasR + " классов ");
         byte Color = (byte) (onePlaceWhite * howClasR);
         byte Color2 = (byte) (onePlaceBrown * howClasR);
         System.out.println(" В школе, где " + howClasR + " классов нужно " + Color + " Банок белой краски и " + Color2 + " Банок коричневой краски");
     }
+    public static void task6 () {
+        byte banansgr = 80;
+        byte milksgr = 105;
+        byte  icecreamgr = 100;
+        byte  egsgr = 70;
+        short  breakfast = (short) (banansgr*5 + milksgr*2 + icecreamgr*2 + egsgr *4);
+        float breakfastkg = (breakfast/1000f);
+        System.out.println(" Столько киллограм продуктов съест спортсмен за утро = "+ breakfastkg + " кг! ");
+    }
+    public static void task7 () {
+        byte totalWeightKg  = 7;
+        short totalWeightGr = (short) (totalWeightKg * 1000);
+        short firstWay = 250;
+        short secondWay = 500;
+        byte totalDayF = (byte) (totalWeightGr / firstWay);
+        System.out.println("Столько дней понадобится спортсмену при первом способе похудения = " + totalDayF + " дней");
+        byte totalDayS = (byte) (totalWeightGr / secondWay);
+        System.out.println("Столько дней понадобится спортсмену при втором способе похудения = " + totalDayS + " дней");
+        byte averageDays = (byte) ((totalDayS+totalDayF)/2);
+        System.out.println("Столько дней в среднем понадобится спортсмену при обоих способах похудения = " + averageDays + " день");
+    }
+    public static void task8 () {
+        int wageMashi  = 67760;
+        int totalYearWageMF = wageMashi * 12;
+        Float wageIncreaseM = wageMashi * 1.1f;
+        int totalYearWageMDop = (int) (wageIncreaseM * 12);
+        int differenceWageOfYearM = totalYearWageMDop - totalYearWageMF;
+        System.out.println(" Маша теперь получает = " +
+                wageIncreaseM + " рублей. " + " Годовой доход вырос на " + differenceWageOfYearM + " рублей! ");
+        int wageDenis  = 83690;
+        int totalYearWageDF = wageDenis * 12;
+        Float wageIncreaseD = wageDenis * 1.1f;
+        int totalYearWageDDop = (int) (wageIncreaseD * 12);
+        int differenceWageOfYearD = totalYearWageDDop - totalYearWageDF;
+        System.out.println(" Денис теперь получает = " +
+                wageIncreaseD + " рублей. " + " Годовой доход вырос на " + differenceWageOfYearD + " рублей! ");
+        int wageKristina  = 76230;
+        int totalYearWageKF = wageKristina * 12;
+        Float wageIncreaseK = wageKristina * 1.1f;
+        int totalYearWageKDop = (int) (wageIncreaseK * 12);
+        int differenceWageOfYearK = totalYearWageKDop - totalYearWageKF;
+        System.out.println(" Кристина теперь получает = " +
+                wageIncreaseK + " рублей. " + " Годовой доход вырос на " + differenceWageOfYearK + " рублей! ");
+    }
 }
+
